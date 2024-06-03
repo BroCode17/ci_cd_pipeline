a2e568d46328e69039e37879e3b190ec7d11a28b
Ebenezer Frimpong <ebenezerfrimpong17@gmail.com>
2024-06-02 20:23:20 -0500

    first commit

---
+++ src/main/java/com/efrimpong/wikimedia/ci_cd_pipeline_tut/CiCdPipelineTutApplication.java
@@ -0,0 +1,13 @@
+package com.efrimpong.wikimedia.ci_cd_pipeline_tut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdPipelineTutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdPipelineTutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/efrimpong/wikimedia/ci_cd_pipeline_tut/CiCdPipelineTutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efrimpong.wikimedia.ci_cd_pipeline_tut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdPipelineTutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
